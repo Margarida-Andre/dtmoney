1310f7896e4eaab8b049376339726d5da041f518
Margarida André <margaridaandre034@gmail.com>
2022-03-16 16:03:03 +0100

    Consome Api fictícia criada com miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transactions 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
